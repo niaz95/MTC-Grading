102654e9e7b56ccf24733c8ed10e4887458ce833
niaz <anmak5@student.monash.edu>
2017-07-25 17:38:04 +1000

    Initial Commit

---
+++ MTC Grading/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MTC Grading
+//
+//  Created by ANM Niaz Makhdum on 25/7/17.
+//  Copyright © 2017 ANM Niaz Makhdum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
